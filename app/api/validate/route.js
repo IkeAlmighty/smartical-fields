d537d850c876fa1c41147a8d56b7fe09fe80ed71
ikealmighty <isaacyates7@gmail.com>
2023-05-17 12:13:08 -0500

    refined openai prompt, fixed code for reading body

--- app/api/validate/route.js
+++ app/api/validate/route.js
@@ -1,28 +1,31 @@
-import { NextResponse } from "next/server"
-import { Configuration } from 'openai';
+import { NextResponse } from "next/server";
+import { Configuration } from "openai";
 import { OpenAIApi } from "openai";
 
 // Initialize OpenAI
-const config = new Configuration({ apiKey: process.env.OPENAI_API_KEY })
-const openai = new OpenAIApi(config)
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
 
 export async function POST(request) {
-    try {
+  try {
+    const body = await request.json();
 
-        // Make the API call to OpenAI
-        const result = await openai.createCompletion({
-            model: 'text-davinci-003',
-            prompt: `Is this an email, name, phone, location, Google map link, or notes: ${request.body.input}`,
-            max_tokens: 10,
-        });
+    // Make the API call to OpenAI
+    const result = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `Respond in one word. Categorize '${body.input}' as either email, name, phone, location, Google map link, letter, or notes.`,
+      max_tokens: 10,
+    });
 
-        // Extract the category from the result
-        const category = result.data.choices[0].text.trim();
+    // Extract the category from the result
+    const category = result.data.choices[0].text.trim();
 
-        return NextResponse.json({ category })
-    } catch (error) {
-        console.log(error.message);
-        return NextResponse.json({ error: error.message });
-    }
-
+    return NextResponse.json({ category });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: error.message });
+  }
 }
